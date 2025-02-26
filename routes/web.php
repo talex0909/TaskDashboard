9cd8f29c9d872aaa9068d07e2cf649762a1faf8e
Alex <malibu999francois@gmail.com>
2025-02-26 12:32:53 +0200

    add create page, route and controller function

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('layout.app');
 })->middleware('auth');
 Route::get('/tasks', [TaskController::class, 'index'])->middleware('auth')->name('tasks.index');
+Route::get('/tasks/add', [TaskController::class, 'create'])->middleware('auth')->name('tasks.create');
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
